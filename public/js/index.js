456d346e1f9aa017079c835aa875c22b98a1b731
Peter King <73837king@gmail.com>
2018-04-27 00:51:33 -0400

    refact splash page html / css

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,15 @@
 'use strict';
 
 const SIGNIN_URL = `/signin`;
-// ************************************************************************* //
-// BLAH - BEGIN
-// ************************************************************************* //
 
 const redirectToSignin = () => {
-  console.log('redirectToSignin ran');
   window.location = SIGNIN_URL;
 };
 
 $(function() {
 
   // Listeners
-  const $signinButton = $('.js-overlay-signin-button');
+  const $signinButton = $('.js-signin-button');
   $signinButton.on('click', redirectToSignin);
 
 });
